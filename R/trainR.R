0d6693a98cd4c19378ee0f899c636cc3d2b96513
stnava <stnava@gmail.com>
2021-11-08 08:32:17 -0500

    ENH: allow heat map return

--- R/trainR.R
+++ R/trainR.R
@@ -44,8 +44,10 @@ loadNPData <- function( numpynames ) {
   np <- import("numpy")
   masknameindex = grep("mask",numpynames)
   ccnameindex = grep("coordconv",numpynames)
-  doMask = length( masknameindex > 0  )
-  doCC = length( ccnameindex > 0  )
+  heatmapnameindex = grep("heatmap",numpynames)
+  doMask = length( masknameindex) > 0
+  doCC = length( ccnameindex ) > 0
+  doHM = length( heatmapnameindex ) > 0
   outlist = list()
   for ( x in 1:length( numpynames ) )
     outlist[[length(outlist)+1]] = np$load( numpynames[ x ] )
@@ -66,8 +68,12 @@ loadNPData <- function( numpynames ) {
 #' @param addCoordConv boolean - generates another array with CoordConv data
 #' @param segmentationsArePoints boolean - converts segmentations to points
 #' @param maskIndex the entry within the list of lists that contains a mask
+#' @param smoothHeatMaps numeric greater than zero will cause method to return heatmaps.
+#' the value passed here also sets the smoothing parameter passed to \code{smoothImage}
+#' in pixel/voxel space.
 #' @param numpynames the names of the numpy on disk files should contain something with
-#' the string mask if using maskIndex and something with the word coordconv if using CC
+#' the string mask if using maskIndex and something with the word coordconv if using CC.
+#' should include something with the word heatmaps if using heatmaps.
 #' @param numberOfSimulations number of output images.  Default = 10.
 #' @param referenceImage defines the spatial domain for all output images.  If
 #' the input images do not match the spatial domain of the reference image, we
@@ -98,7 +104,17 @@ loadNPData <- function( numpynames ) {
 #' @importFrom ANTsRNet dataAugmentation
 #' @importFrom reticulate import
 #' @examples
-#' mydf = NULL
+#' library( ANTsR )
+#' ilist = list( list( ri(1) ), list( ri(2) ) )
+#' slist = list(
+#'   thresholdImage( ilist[[1]][[1]], "Otsu",3),
+#'   thresholdImage( ilist[[2]][[1]], "Otsu",3) )
+#' npn = paste0(tempfile(), c('i.npy','s.npy','heatmap.npy') )
+#' temp = generateDiskData( ilist, slist, c(0:3), c(TRUE,TRUE), numpynames = npn  )
+#' temp = generateDiskData( ilist, slist, c(0:3), c(TRUE,TRUE),
+#'   segmentationsArePoints=TRUE, numpynames = npn )
+#' temp = generateDiskData( ilist, slist, c(0:3), c(TRUE,TRUE),
+#'   segmentationsArePoints=TRUE, smoothHeatMaps = 3.0, numpynames = npn )
 #' @export
 generateDiskData  <- function(
   inputImageList,
@@ -108,6 +124,7 @@ generateDiskData  <- function(
   addCoordConv=0,
   segmentationsArePoints = FALSE,
   maskIndex,
+  smoothHeatMaps = 0,
   numpynames,
   numberOfSimulations = 16,
   referenceImage = NULL,
@@ -134,6 +151,12 @@ generateDiskData  <- function(
   } else {
     Y = array( dim = c( numberOfSimulations, nClasses, myimgdim ) )
   }
+  if ( smoothHeatMaps > 0 & segmentationsArePoints ) {
+    Yh = array( 0, dim = c( numberOfSimulations, idim, nClasses ) )
+    if ( length( grep("heatmap",numpynames) ) == 0 )
+      stop( "numpynames must have a name containing the string heatmap" )
+    heatmapnameindex = grep("heatmap",numpynames)
+  }
   if (! missing( maskIndex ) ) {
     doMask = TRUE
     Xm = array( dim = c( numberOfSimulations, idim, 1 ) )
@@ -150,6 +173,7 @@ generateDiskData  <- function(
       stop( "numpynames must have a name containing the string coordconv" )
     ccnameindex = grep("coordconv",numpynames)
     }
+
 
   data <- dataAugmentation(
     inputImageList[selector],
@@ -192,6 +216,15 @@ generateDiskData  <- function(
           temp = thresholdImage( data$simulatedSegmentationImages[[k]][[1]],
             segmentationNumbers[j], segmentationNumbers[j]  )
           mypt = getCentroids( temp, clustparam=0  )
+          if ( smoothHeatMaps > 0 & ( length(mypt) > 0 ) ) {
+            mypti = antsTransformPhysicalPointToIndex( temp, mypt[,1:myimgdim] )
+            heatmap = temp * 0.0
+            if ( myimgdim == 2 ) heatmap[mypti[1,1],mypti[1,2]]=1
+            if ( myimgdim == 3 ) heatmap[mypti[1,1],mypti[1,2],mypti[1,3]]=1
+            heatmap = smoothImage( heatmap, smoothHeatMaps, sigmaInPhysicalCoordinates=FALSE )
+            if ( myimgdim == 2 ) Yh[k, , , j ] = as.array( heatmap )
+            if ( myimgdim == 3 ) Yh[k, , , , j ] = as.array( heatmap )
+            }
           if ( length(mypt) == 0 ) mypt = rep( NA, myimgdim )
           Y[k, j, ] = mypt[1:myimgdim]
           }
@@ -204,9 +237,13 @@ generateDiskData  <- function(
     np$save( numpynames[masknameindex], Xm )
     outlist[[length(outlist)+1]] = Xm
     }
-  if ( doCC ) {
+  if ( doCC > 0 ) {
     np$save( numpynames[ccnameindex], Xcc )
     outlist[[length(outlist)+1]] = Xcc
+    }
+  if ( smoothHeatMaps > 0 ) {
+    np$save( numpynames[heatmapnameindex], Yh )
+    outlist[[length(outlist)+1]] = Yh
     }
   return( outlist  )
   }
